edbb9c49ca066c07b0299a6060475f90afb34f1b
David Fuhry <dfuhry@twitter.com>
2013-10-13 20:28:25 -0700

    add script to run label propagation with different parameter configurations

---
+++ run_label_propagation.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+examples/scala/label_propagation.sh 0.1 1
+examples/scala/label_propagation.sh 0.1 2
+examples/scala/label_propagation.sh 0.1 3
+examples/scala/label_propagation.sh 0.1 4
+examples/scala/label_propagation.sh 0.1 8
+examples/scala/label_propagation.sh 0.1 0
+
+examples/scala/label_propagation.sh 0.2 1
+examples/scala/label_propagation.sh 0.2 2
+examples/scala/label_propagation.sh 0.2 3
+examples/scala/label_propagation.sh 0.2 4
+examples/scala/label_propagation.sh 0.2 8
+examples/scala/label_propagation.sh 0.2 0
+
+examples/scala/label_propagation.sh 0.05 1
+examples/scala/label_propagation.sh 0.05 2
+examples/scala/label_propagation.sh 0.05 3
+examples/scala/label_propagation.sh 0.05 4
+examples/scala/label_propagation.sh 0.05 8
+examples/scala/label_propagation.sh 0.05 0
+
+examples/scala/label_propagation.sh 0.3 1
+examples/scala/label_propagation.sh 0.3 2
+examples/scala/label_propagation.sh 0.3 3
+examples/scala/label_propagation.sh 0.3 4
+examples/scala/label_propagation.sh 0.3 8
+examples/scala/label_propagation.sh 0.3 0
+
